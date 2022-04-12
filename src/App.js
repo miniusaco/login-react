067b52bd695904c4ffed7b996e2a64a475d3fadd
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-04-12 22:36:55 +0900

    ログインフォームを作る

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input type="text" placeholder="ユーザー名" name="username" />
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input
+              type="text"
+              placeholder="メールアドレス"
+              name="mailAddress"
+            />
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder="パスワード" name="password" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
